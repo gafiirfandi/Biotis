0d1ec16e6bf1949088347f5eae878582041f488c
DestaraSyanandi <destarasyanando@gmail.com>
2020-08-23 15:12:55 +0700

    adding app login 60%

--- login/urls.py
+++ login/urls.py
@@ -1,12 +1,10 @@
-from django.contrib import admin
+  
 from django.urls import path
-from .views import login
-from django.conf import settings
-from django.conf.urls.static import static
-
+from . import views
 
 app_name = 'login'
 
 urlpatterns = [
-    path('',login,name='login'),
+    path('register/', views.registerPage, name="register"),
+	path('loginPage/', views.loginPage, name="loginPage"),  
 ]
