d05398006a50b846c7ff2041a3e54f2f61819fbe
Lado Rayhan Najib <ladorayhannajib@gmail.com>
2020-11-15 16:25:23 +0700

    verification account and adding reset password

--- login/urls.py
+++ login/urls.py
@@ -6,6 +6,8 @@ app_name = 'login'
 
 urlpatterns = [
     path('register/', views.registerPage, name="register"),
-	path('', views.loginPage, name="loginPage"),  
+    path('reset/', views.reset, name="reset"),
+	path('', views.loginPage, name="loginPage"),
+    path('activate/<uidb64>/<token>/',views.activate, name='activate'),  
     path('logout/', views.logoutUser, name="logout")
 ]
