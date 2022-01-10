6475468d89d6fbaa917a1dcfdc618f3cc43c3e45
Ali Irsyaad Nursyaban <ali.nursyaban@tokopedia.com>
2022-01-10 20:40:24 +0700

    fix database setting

--- biotis/settings.py
+++ biotis/settings.py
@@ -82,9 +82,13 @@ WSGI_APPLICATION = 'biotis.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'biotisst_db',
+        'USER': 'biotisst_admin',
+        'PASSWORD': '8^j)d%;yzva9',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
