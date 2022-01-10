20dfa385f0d107d7947ac87ea996b895ede28c17
Lado <lado.intern@mail.sirclo.com>
2022-01-10 22:39:10 +0700

    revert setting static root

--- biotis/settings.py
+++ biotis/settings.py
@@ -136,7 +136,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = 'home/biotisst/public_html/static'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
